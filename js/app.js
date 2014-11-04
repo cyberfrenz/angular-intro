2f15c05d6f840afdc6584e8a2f48cc6bbfc096c9
Antony <antonz@ue.edu>
2014-11-04 10:17:00 -0800

    fooling around

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@
 
 "use strict";
 
+angular.module('MoviesApp', [])
+	.controller('MoviesController', function($scope) {
+		
+		$scope.movies = movies;
+
+		// set up our sort
+		$scope.sortCol = 'rank';
+		$scope.sortReverse = false;
+
+		$scope.sortBy = function(colName) {
+			if($scope.sortCol == colName) {
+				$scope.sortReverse = !$scope.sortReverse;
+			} else {
+				$scope.sortReverse = false; 
+				$scope.sortCol = colName;
+			}
+		};
+
+		$scope.isSortedBy = function(colName) {
+			return $scope.sortCol == colName;
+		}
+ 	});
